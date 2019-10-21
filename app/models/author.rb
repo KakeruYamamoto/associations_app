dd526f13bb7a80a8283d3f7f526392c4093d340f
kakeru <okinawan.kkr0713@icloud.com>
2019-10-21 20:01:07 +0900

    controller/blog

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,8 @@
 class Author < ApplicationRecord
   has_many :books#結びつけたいテーブルのモデルを指定。複数のhas_manyと単数のbelongs_to
 end#書籍は複数
+
+
+#アソシエーションで定義されたものがメソッドになる。
+
+# author.books
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :author#結びつけたいテーブルのモデルを指定。複数のhas_manyと単数のbelongs_to
 end #著者は一人
+
+
+#アソシエーションで定義されたものがメソッドになる。

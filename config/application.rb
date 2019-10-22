b58ebf6247470b80dac0bde5d171244ea86b9a20
kakeru <okinawan.kkr0713@icloud.com>
2019-10-22 12:48:38 +0900

    config/applicationタイムゾーン記述

--- config/application.rb
+++ config/application.rb
@@ -2,22 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module AssociationsApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.time_zone = 'Tokyo'  #タイムゾーンとはこのアプリケーションに設定される時間のことです 
+    config.time_zone = 'Tokyo'  #タイムゾーンとはこのアプリケーションに設定される時間のことです
     config.active_record.default_timezone = :local#created_atなどのデータが保存される際、その時間を参考にして時間データが保存されます
 
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
